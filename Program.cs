78ca9856313d591d7f70ce70d8aa20ab83983b30
Kilastly <kilastly@gmail.com>
2023-12-17 16:23:03 +0600

    Решение задачи 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+*/
+void IntIn(int max, int min)
+{
+     Console.Write($"{max} ");
+    if (max == min)
+    {
+    return;
+    }
+        IntIn(max-1, min);
+       
+    
+}
+
+Console.Clear();    
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a < b)
+    {
+        IntIn(b, a);
+    }
+else
+{
+IntIn(a, b);
+}
